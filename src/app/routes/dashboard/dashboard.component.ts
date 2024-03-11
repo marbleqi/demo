cd2ead1b181221446a67b85c6d829dbc7c256ec1
marbleqi <marbleqi@163.com>
2024-03-11 11:37:12 +0800

    调整变更检测处理逻辑

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { PageHeaderModule } from '@delon/abc/page-header';
 import { OnReuseInit } from '@delon/abc/reuse-tab';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [PageHeaderModule]
 })
 export class DashboardComponent implements OnInit, OnReuseInit {
+  private readonly cdr = inject(ChangeDetectorRef);
   info: string = '仪表板页面';
 
   /**页面初始化 */
   ngOnInit(): void {
     this.info = '仪表板页面初始化！！';
+    this.cdr.markForCheck();
     console.debug(this.info);
   }
 
   /**路由复用初始化 */
   _onReuseInit() {
     this.info = '仪表板页面路由复用初始化！！';
+    this.cdr.markForCheck();
     console.debug(this.info);
   }
 }
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { PageHeaderModule } from '@delon/abc/page-header';
 import { OnReuseInit } from '@delon/abc/reuse-tab';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [PageHeaderModule]
 })
 export class HomeComponent implements OnInit, OnReuseInit {
+  private readonly cdr = inject(ChangeDetectorRef);
   info: string = '首页页面';
 
   /**页面初始化 */
   ngOnInit(): void {
     this.info = '首页页面初始化！！';
+    this.cdr.markForCheck();
     console.debug(this.info);
   }
 
   /**路由复用初始化 */
   _onReuseInit() {
     this.info = '首页页面路由复用初始化！！';
+    this.cdr.markForCheck();
     console.debug(this.info);
   }
 }
